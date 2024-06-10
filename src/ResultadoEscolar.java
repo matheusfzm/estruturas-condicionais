2307a7bedbdcdf1e3e8133c112a5575b229f255e
Matheus Macedo <zanellamatheus15@gmail.com>
2024-06-10 10:30:32 -0300

    condição ternária exemplos 1 e 2

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,5 +1,6 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
+       /*
        int nota = 5;
        
        if(nota >= 7)
@@ -10,5 +11,22 @@ public class ResultadoEscolar {
 
        else
         System.out.println("Reprovado");
+        */
+
+
+        /*Exemplo 1 condicional encadeada
+        int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+        */
+
+        /*Exemplo 2 
+        int nota = 3;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);   
+        */
+
+
+
     }
 }
