7a4d9ca7e2beb14a500432fd94f1f776ed061083
Matheus Macedo <zanellamatheus15@gmail.com>
2024-06-10 10:19:32 -0300

    condicionais encadeadas

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,9 +1,12 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-       int nota = 8;
+       int nota = 5;
        
        if(nota >= 7)
         System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota <7)
+            System.out.println("Prova de Recuperação");
 
        else
         System.out.println("Reprovado");
