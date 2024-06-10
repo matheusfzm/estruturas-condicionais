a320cf7cc1c2e2d98d275cdf228a3120605c10f5
Matheus Macedo <zanellamatheus15@gmail.com>
2024-06-10 11:46:26 -0300

    utilizando switch case - 1

---
+++ src/SistemaMedida.java
@@ -0,0 +1,44 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        /*Exemplo1 utilizando if e else
+        
+        String sigla = "G";
+
+		    if(sigla == "P")
+			    System.out.println("PEQUENO");
+		    else if(sigla == "M")
+			    System.out.println("MÉDIO");
+		    else if(sigla == "G")
+			    System.out.println("GRANDE");
+		    else
+			    System.out.println("INDEFINIDO");
+			*/
+
+
+            /*Exemplo 2 utilizando Switch Case, porém sem necessidade
+            String sigla = "GG";
+            switch (sigla) {
+            case "P":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M":{
+                System.out.println("MÉDIO");
+                break;
+            }
+            case "G":{
+                System.out.println("GRANDE");
+                break;
+            }
+            default:
+                System.out.println("INDEFINIDO");
+            }
+            */
+
+
+            
+
+
+
+	}
+}
